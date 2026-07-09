4aab828037089f13abcafbff45a64ef849404b46
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-07-09 13:54:19 +0200

    Update Footer.tsx
    updated footer location mark

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,5 @@
 import { FaLocationDot } from 'react-icons/fa6'
+// import { services } from '@/data/services'
 
 export default function Footer() {
     return (
@@ -14,16 +15,15 @@ export default function Footer() {
                 <p className="flex justify-between">Mieczysław: <a href="tel:+48123456789" className="text-red-500 hover:underline">+48 123 456 789</a></p>
                 <p className="flex justify-between">Robert: <a href="tel:+48123456789" className="text-red-500 hover:underline">+48 123 456 789</a></p>
                 <p className="flex justify-between">Rafał: <a href="tel:+48123456789" className="text-red-500 hover:underline">+48 123 456 789</a></p>
-                <a href="https://maps.app.goo.gl/BZLzjFTBMpkseLKw8" target="_blank" rel="noopener noreferrer" className={` text-neutral-300 font-sans  flex items-center gap-2 mt-6`}> <FaLocationDot className='text-red-500' />&nbsp;21-400 Łuków, ul. Radzyńska 4</a>
+                <a href="https://maps.app.goo.gl/BZLzjFTBMpkseLKw8" target="_blank" rel="noopener noreferrer" className={` text-neutral-300 font-sans  flex items-center gap-2 mt-6`}> <FaLocationDot className='text-red-500 p-1' />21-400 Łuków, ul.&nbsp;Radzyńska&nbsp;4</a>
                 
             </div>
             <div>
                 <h3 className={`text-2xl font-bold mb-2 md:mb-4 text-neutral-100`}>Usługi</h3>
                 <ul className="list-disc list-inside ">
-                    <li className="text-neutral-300 list-disc ">Diagnostyka samochodowa</li>
-                    <li className="text-neutral-300 list-disc ">Remont silników</li>
-                    <li className="text-neutral-300 list-disc ">Usługi serwisowe</li>
-                    <li className="text-neutral-300 list-disc ">Naprawa karoserii</li>
+                    {/* {services.map((service, index) => (
+                        <li key={index} className="mb-1">{service}</li>
+                    ))} */}
                 </ul>
             </div>
         </footer>
