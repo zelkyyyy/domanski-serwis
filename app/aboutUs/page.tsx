a7d00fdc25e89443f08d6832db140952a86751b2
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-07-12 02:20:39 +0200

    Added subpages Services and About Us

---
+++ app/aboutUs/page.tsx
@@ -0,0 +1,9 @@
+import About from "../components/About";
+
+export default function AboutUs() {
+    return (
+        <div className="flex min-h-screen">
+            <About />
+        </div>
+    )
+}
---
+++ app/services/page.tsx
@@ -0,0 +1,9 @@
+import ServicesComponent from "../components/Services";
+
+export default function Services() {
+    return (
+        <div className="flex min-h-screen">
+            <ServicesComponent />
+        </div>
+    )
+}
