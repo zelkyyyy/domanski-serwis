4cbc4543ef1f0bf50d94840f31e5663037e7bfad
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-07-12 02:20:19 +0200

    Renamed Main to Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,20 +4,19 @@ import Image from 'next/image';
 import { FaLocationDot } from "react-icons/fa6";
 
 
-const Main = () => {
+const Hero = () => {
   return (
-    <div id="home" className="relative z-0 w-full h-screen flex items-center justify-center shadow-[0_30px_50px_rgba(0,0,0,1)]">   {/* <- KONTEKST */}
+    <div id="home" className="relative flex min-h-screen w-full items-center justify-center overflow-hidden shadow-[0_30px_50px_rgba(0,0,0,1)]">
       <Image
-        className="absolute select-none  blur-sm inset-0 w-full h-full object-cover grayscale-100 brightness-50"
+        className="absolute h-full w-full inset-0  select-none object-cover blur-sm grayscale-100 brightness-50"
         alt="Background"
         src="/images/background.png"
         width={600}
         height={200}
         draggable={false}
-        
       />
-      {/* <div className="absolute inset-0 w-full h-full bg-black opacity-60 flex items-center justify-center" /> */}
-      <div className="relative z-10 flex w-[90%] max-w-[800px] flex-col items-center justify-center gap-2 my-auto text-neutral-100">
+
+      <div className="relative z-10 flex w-[90%] max-w-[800px] flex-col items-center justify-center gap-2 px-4 py-12 text-neutral-100">
         <Image className='w-full h-auto select-none shadow-black brightness-80' src="/images/logo-white1.png" alt="Domańscy Autoserwis" width={800} height={300} draggable={false} />
         <span className="block w-full bg-linear-to-br from-neutral-200 from-30% to-neutral-400/70 bg-clip-text text-center text-[clamp(2rem,5vw,3.75rem)] font-bold font-sans leading-none tracking-wide text-transparent">
           Domańscy Autoserwis
@@ -30,10 +29,12 @@ const Main = () => {
             <a href="tel:" className={`text-xl text-white font-bold font-sans px-6 py-4 rounded-lg bg-red-600 hover:shadow-[0_0_40px_var(--color-red-600)] shadow-red-500/60 duration-300 flex items-center gap-2 `}><BsFillTelephoneFill />Mieczysław</a>
             <a href="tel:" className={`text-xl text-white font-bold font-sans px-6 py-4 rounded-lg bg-red-600 hover:shadow-[0_0_40px_var(--color-red-600)] shadow-red-500/60 duration-300 flex items-center gap-2 `}><BsFillTelephoneFill />Rafał</a>
         </div>
-        <span className={`text-xl text-neutral-300 font-sans  flex items-center gap-2 mt-10`}><FaLocationDot className='text-red-500' />&nbsp;21-400 Łuków, ul. Radzyńska 4</span>
+        <a href="https://maps.app.goo.gl/BZLzjFTBMpkseLKw8" target="_blank" rel="noopener noreferrer" className={`text-xl text-neutral-300 font-sans  flex items-center gap-2 mt-10`}>
+          <FaLocationDot className='text-red-500' />&nbsp;21-400 Łuków, ul. Radzyńska 4
+        </a>
       </div>
     </div>
   )
 }
 
-export default Main
+export default Hero
