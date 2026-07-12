0b806439326d1bf70aac1364ce4264c09b53b9a7
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-07-12 02:21:49 +0200

    Fixed position of Navbar and added links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import NavbarLink from './NavbarLink'
 import { BsTelephone} from "react-icons/bs";
 import { useEffect, useState } from 'react';
 import HamburgerMenu from './HamburgerMenu'
-
+import { usePathname } from 'next/navigation';
 
 
 
@@ -21,15 +21,17 @@ const Navbar = () => {
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
+    const pathname = usePathname();
+
   return (
     <>
-    <div className={`hidden lg:flex fixed  z-80 backdrop-blur-md items-center justify-between py-4 px-24 bg-[##121212] w-full mx-auto shadow-xl shadow-black/25 border-b border-neutral-200/30 ${atTop ? 'h-32' : 'h-20'} transition-all duration-500`}>
+    <div className={`hidden lg:flex top-0  z-80 backdrop-blur-md items-center justify-between py-4 px-24 bg-[##121212] w-full mx-auto shadow-xl shadow-black/25 border-b border-neutral-200/30 ${atTop ? 'h-24' : 'h-20'} transition-all duration-500 ${pathname === '/' ? 'fixed' : 'sticky'}`}>
       <Image className={`hover:-translate-x-96 duration-500 delay-1000 w-auto h-full`} alt='logo domanski serwis' src="/images/logo.png" width={400} height={200} draggable={false} />
       <div className="space-x-6">
-                    <NavbarLink href="#" name="Strona główna" />
-                    <NavbarLink href="#o-nas" name="O nas" />
-                    <NavbarLink href="#usługi" name="Usługi" />
-                    <NavbarLink href="#kontakt" name="Kontakt" />
+                    <NavbarLink href="/" name="Strona główna" />
+                    <NavbarLink href="/aboutUs" name="O nas" />
+                    <NavbarLink href="/services" name="Usługi" />
+                    <NavbarLink href="/contact" name="Kontakt" />
       </div>
       <a href="tel:" className={`hover:bg-neutral-100 bg-red-600 text-neutral-100 rounded-lg ${atTop ? "h-16" : "h-12"} px-6 py-3 text-xl font-bold hover:text-red-600 duration-300 flex items-center gap-2 hover:shadow-[0_0_40px_var(--color-neutral-200)] hover:shadow-neutral-200/50`}>
         <BsTelephone className='font-extrabold text-lg' />
