c3156532ba23e45c95f16e142cd08c083d9a5e07
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-07-21 11:11:16 +0200

    Updated paths for api

--- app/api/services/route.ts
+++ app/api/services/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import services from '@/public/data/services.json';
+import services from '@/data/services.json';
 
 export async function GET() {
     return NextResponse.json(services);
