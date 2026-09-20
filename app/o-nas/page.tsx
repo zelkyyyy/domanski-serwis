1284e9086d1ffe28e789b538ceabdc2a7694185d
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-09-20 17:13:20 +0200

    Add background gradient

--- app/o-nas/page.tsx
+++ app/o-nas/page.tsx
@@ -1,9 +1,10 @@
 import About from "../components/About";
 
 export default function AboutUs() {
-    return (
-        <div className="flex min-h-screen">
-            <About />
-        </div>
-    )
+  return (
+    <div className="relative flex min-h-screen">
+        <div className="absolute -left-1/2 top-0 inset-0 bg-radial from-neutral-800/50 to-70%  to-transparent" />
+      <About />
+    </div>
+  );
 }
--- app/uslugi/page.tsx
+++ app/uslugi/page.tsx
@@ -1,9 +1,12 @@
 import ServicesComponent from "../components/Services";
+import services from "../../data/services.json";
 
 export default function Services() {
-    return (
-        <div className="flex min-h-screen">
-            <ServicesComponent />
-        </div>
-    )
+  return (
+    <div className="relative flex min-h-screen">
+      <div className="absolute -left-1/2 top-0 inset-0 bg-radial from-neutral-800/50 to-80%  to-transparent" />
+
+      <ServicesComponent services={services} />
+    </div>
+  );
 }
