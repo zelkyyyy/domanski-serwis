41d2823ceb2ddaebbe661c50cbcee8040f057f92
Maciej Owczarski <maciej.owczarski@gmail.com>
2026-09-20 17:10:59 +0200

    Delete API route

--- app/api/services/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import services from '@/data/services.json';
-
-export async function GET() {
-    return NextResponse.json(services);
-}
-
--- app/api/telephones/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import telephones from '@/data/telephones.json';
-
-export async function GET() {
-    return NextResponse.json(telephones);
-}
-
